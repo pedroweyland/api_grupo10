6e24941e22e8611456b735609279377ae3ac04b8
pedroweyland <weylandpedro@gmail.com>
2024-09-09 17:44:00 -0300

    Feat: Creacion de Server.js

---
+++ models/server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+
+class Server {
+    constructor () {
+      this.app = express()
+      this.port = process.env.PORT || 3000
+      this.middleware()
+      this.rutas()
+    }
+  
+    middleware () {
+      this.app.use(express.static('public'))
+    }
+  
+    rutas () {
+      //this.app.use('/api/v1/peliculas', require('../routes/peliculas')) // Integrante 1
+      
+      // Daniel D'Onofrio
+
+      // Gabriel Ponce
+
+      // Galo Santopietro
+
+      // Pedro Weyland
+
+    }
+  
+    listen () {
+      this.app.listen(this.port, () => {
+        console.log(`La API esta escuchando en el this.PORT ${this.port}`)
+      })
+    }
+}
+  
+module.exports = Server
