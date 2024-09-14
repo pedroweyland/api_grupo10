7d53685974e93d34274267759c72eb04d4c75605
pedroweyland <weylandpedro@gmail.com>
2024-09-13 22:15:28 -0300

    Fix: Agrego mi URL a mi .env

--- controllers/proximamente.js
+++ controllers/proximamente.js
@@ -1,12 +1,9 @@
 const axios = require('axios')
 const { request, response } = require('express')
 
-// Guardo la url de mi API que voy a consultar en este caso themoviedb
-const url = 'https://api.themoviedb.org/3/movie/'
-
 // Funcion para tener una lista de las peliculas que van a salir proximamente
 const getProximamente = (req = request, res = response) => {
-  axios.get(url + 'upcoming', {
+  axios.get(process.env.url + 'movie/upcoming', {
     params: {
       api_key: process.env.API_KEY
     }
@@ -16,15 +13,15 @@ const getProximamente = (req = request, res = response) => {
       const { data } = response
 
       res.status(200).json({
-        msg: '200K',
-        data
+        status: 200,
+        data: data.results
       })
     })
     .catch((error) => {
       // Si ocurre un Error lo atrapo y le mando al usuario el codigo de error y lo que sucedio
       if (error.response.status === 404) {
         res.status(400).json({
-          statusCode: 400,
+          status: 400,
           messege: 'Bad Request - Error inesperado'
         })
       }
@@ -35,7 +32,7 @@ const getProximamente = (req = request, res = response) => {
 const getCreditosDePelicula = (req = request, res = response) => {
   const { idPelicula = '' } = req.params
 
-  axios.get(url + idPelicula + '/credits', {
+  axios.get(process.env.url + 'movie/' + idPelicula + '/credits', {
     params: {
       api_key: process.env.API_KEY
     }
@@ -44,20 +41,20 @@ const getCreditosDePelicula = (req = request, res = response) => {
       const { data } = response
       // Si sale todo bien le entrego al usuario los creditos y con un condigo 200K
       res.status(200).json({
-        msg: '200K',
-        data
+        status: 200,
+        data: data.cast
       })
     })
     .catch((error) => {
       // Si ocurre un Error lo atrapo y le mando al usuario el codigo de error y lo que sucedio
       if ((error.response.status === 404)) {
         res.status(404).json({
-          statusCode: 404,
+          status: 404,
           messege: 'NOT FOUND - Pelicula no encontrada'
         })
       } else {
         res.status(400).json({
-          statusCode: 400,
+          status: 400,
           messege: 'BAD REQUEST - Error inesperado'
         })
       }
