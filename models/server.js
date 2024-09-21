05089cf12d19697d331a99c984e942d7048d6da8
pedroweyland <weylandpedro@gmail.com>
2024-09-21 18:30:34 -0300

    Fix: Ejecuto lint:fix

--- models/server.js
+++ models/server.js
@@ -1,34 +1,33 @@
 const express = require('express')
 
 class Server {
-    constructor () {
-      this.app = express()
-      this.port = process.env.PORT || 3000
-      this.middleware()
-      this.routes()
-    }
-  
-    middleware () {
-      this.app.use(express.static('public'))
-    }
-  
-    routes () {
-      //this.app.use('/api/v1/peliculas', require('../routes/peliculas')) // Integrante 1
-      
-      // Daniel D'Onofrio
+  constructor () {
+    this.app = express()
+    this.port = process.env.PORT || 3000
+    this.middleware()
+    this.routes()
+  }
 
-      // Gabriel Ponce
+  middleware () {
+    this.app.use(express.static('public'))
+  }
 
-      // Galo Santopietro y Pedro Weyland
-      this.app.use('/api/v1/movie', require('../routes/movie'))
+  routes () {
+    // this.app.use('/api/v1/peliculas', require('../routes/peliculas')) // Integrante 1
 
-    }
-  
-    listen () {
-      this.app.listen(this.port, () => {
-        console.log(`La API esta escuchando en el this.PORT ${this.port}`)
-      })
-    }
+    // Daniel D'Onofrio
+
+    // Gabriel Ponce
+
+    // Galo Santopietro y Pedro Weyland
+    this.app.use('/api/v1/movie', require('../routes/movie'))
+  }
+
+  listen () {
+    this.app.listen(this.port, () => {
+      console.log(`La API esta escuchando en el this.PORT ${this.port}`)
+    })
+  }
 }
 
 module.exports = Server
