dcac418a3d05b511bc2d7d12983812b3c468e04a
Daniel D'Onofrio <danieldonofrio@live.com>
2025-03-15 22:09:38 -0300

    fix

--- src/service/popular_series.js
+++ src/service/popular_series.js
@@ -1,27 +1,26 @@
 import axios from 'axios'
 
 const fetchPopularSeries = async (queryParams) => {
-    const filter = queryParams.length > 0 ? `?${queryParams.join('&')}` : ''
-  
-    try {
-      const response = await axios.get(`${process.env.URL}tv/popular${filter}`, {
-        params: {
-          api_key: process.env.API_KEY
-        }
-      })
-      if (response.data.results.length === 0) {
-        const error = new Error('NOT FOUND - Serie not found')
-        error.status = 404
-        throw error
+  const filter = queryParams.length > 0 ? `?${queryParams.join('&')}` : ''
+
+  try {
+    const response = await axios.get(`${process.env.URL}tv/popular${filter}`, {
+      params: {
+        api_key: process.env.API_KEY
       }
-      return response.data
-    } catch (error) {
-      error.status = error.response?.status || 400
+    })
+    if (response.data.results.length === 0) {
+      const error = new Error('NOT FOUND - Serie not found')
+      error.status = 404
       throw error
     }
+    return response.data
+  } catch (error) {
+    error.status = error.response?.status || 400
+    throw error
   }
-  
-  export {
-    fetchPopularSeries
-  }
-  
+}
+
+export {
+  fetchPopularSeries
+}
