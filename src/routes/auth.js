05ba46a17495b03ea12dbfb403a5c591815f2615
Pedro Weyland <weylandpedro@gmail.com>
2025-03-26 20:14:40 -0300

    Feat: Creo endpoint de lista de usuarios

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import postRegister from '../controllers/register.js'
 import postLogin from '../controllers/login.js'
+import getUsers from '../controllers/list_users.js'
 
 const routes = Router()
 
 routes.post('/register', postRegister)
 routes.post('/login', postLogin)
+routes.get('/users', getUsers)
 
 export default routes
